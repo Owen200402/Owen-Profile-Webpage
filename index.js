205762ae170778c19a53a9db872d25cfff1707a6
Owen Zheng <johndoe@example.com>
2021-08-19 19:11:07 -0700

    Added preloader

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
+
+window.addEventListener("load", () => {
+  document.querySelector(".preloader").classList.add("hide-preloader");
+});
